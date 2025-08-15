1247089f80f4001bb586c0e3de10349d4f4ce999
brhiza <brhiza@qq.com>
2025-08-15 20:10:49 +0800

    233

--- script.js
+++ script.js
@@ -506,3 +506,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.service.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
